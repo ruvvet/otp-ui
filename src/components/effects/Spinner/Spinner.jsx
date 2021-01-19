9fa6802e766e20acda771559cd687300eda26cc6
Jenny <finej01@gmail.com>
2021-01-19 11:45:11 -0500

    add spinner component

---
+++ src/components/effects/Spinner/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './spinner.css';
+
+import CircularProgress from '@material-ui/core';
+
+export default function Spinner() {
+  return <CircularProgress color="secondary" />;
+}
